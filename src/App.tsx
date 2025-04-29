e24a6aa8bca685d28324ed800acaa07927e6bf4d
Arela-Cristina <instagrandine@gmail.com>
2025-04-29 15:34:08 +0200

    feat: create MainLayout component to use Outlet and nested routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-import Header from "./components/Header";
+import MainLayout from "./pages/MainLayout";
+import Home from "./pages/Home";
+import BrawlersList from "./pages/BrawlersList";
+import FavoriteBrawlers from "./pages/FavoriteBrawlers";
 
 function App() {
 
 
   return (
-    <>
-      <Header />
-      <div>Hello Brawl</div>
-    </>
 
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="BrawlersList" element={<BrawlersList />} />
+          <Route path="FavoriteBrawlers" element={<FavoriteBrawlers />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+
+  );
 }
 
 export default App

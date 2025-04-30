ffd8829163f89ec897865bb450b48caca7fc9da8
Arela-Cristina <instagrandine@gmail.com>
2025-04-30 18:14:33 +0200

    feat: use BrawlersContext to render data in BrawlersList component

--- src/pages/BrawlersList.tsx
+++ src/pages/BrawlersList.tsx
@@ -1,7 +1,34 @@
-import { useState, useEffect } from "react"
+import { useContext } from "react"
+import BrawlersContext from "../global/BrawlersContext"
+import { Brawler } from "../types/types"
+import style from './ui-pages-styles/BrawlerList.module.css'
 
 export default function BrawlersList() {
+
+    const brawlerContext = useContext(BrawlersContext)
+
+
+    if (brawlerContext === null) {
+        return <div>Loading...</div>
+    }
+
+    const { brawler } = brawlerContext
+
     return (
-        <div>Lista di Brawlers</div>
+
+        <section className={style.mainListContainer}>
+            <h2>Lista di Brawlers</h2>
+            <ul className={style.listContainer}>
+                {brawler.map((b: Brawler, id: number) => (
+                    <li key={id} className={style.listElement}>
+                        <figure>
+                            <div>{b.title}</div>
+                            <p>{b.category}</p>
+                        </figure>
+                    </li>
+                ))}
+            </ul>
+        </section>
+
     )
 }

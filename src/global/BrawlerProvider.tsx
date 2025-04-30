c66f81068ac063fea4b96bfe327eaba5367812f3
Arela-Cristina <instagrandine@gmail.com>
2025-04-30 16:54:25 +0200

    feat: fetch Brawlers and implement udeEffect to improve performance

--- src/global/BrawlerProvider.tsx
+++ src/global/BrawlerProvider.tsx
@@ -1,6 +1,9 @@
-import { ReactNode, useState } from "react"
+import { ReactNode, useState, useEffect } from "react"
 import BrawlersContext from "./BrawlersContext"
 import { Brawler } from "../types/types"
+
+
+const BASE_API = import.meta.env.VITE_API_URL;
 
 interface Props {
     children: ReactNode
@@ -9,6 +12,35 @@ interface Props {
 export const BrawlersProvider = ({ children }: Props) => {
 
     const [brawler, setBrawler] = useState<Brawler[]>([])
+
+    useEffect(() => {
+
+        const fetchBrawlers = async () => {
+
+            try {
+
+                const response = await fetch(`${BASE_API}/brawlers`);
+
+                if (!response.ok) {
+                    throw new Error(`Errore nella chiamata`)
+                }
+
+                const data: Brawler[] = await response.json()
+                console.log(`data`, data)
+                setBrawler(data)
+
+            } catch (error) {
+                if (error instanceof Error) {
+                    console.error(error.message)
+                } else {
+                    console.log(error)
+                }
+            }
+        };
+
+        fetchBrawlers()
+    }, [])
+
 
     return (
         <BrawlersContext.Provider value={{ brawler, setBrawler }}>

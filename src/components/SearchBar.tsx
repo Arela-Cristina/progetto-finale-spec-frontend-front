1f6e281222bf270c8fcc51c928af24e887d07005
Arela-Cristina <instagrandine@gmail.com>
2025-05-01 19:08:06 +0200

    feat: connect to global SearchContext using useContext and destructure props in SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,31 @@
+import { useContext } from "react"
+import SearchContext from "../global/SearchContext"
+
+
 export default function SearchBar() {
+
+    const SearchQuerycontext = useContext(SearchContext)
+
+    if (!SearchQuerycontext) {
+        return null
+    }
+
+    const { searchQuery, SetSearchQuery, sortOrder, setSortOrder } = SearchQuerycontext
+
     return (
         <>
             <div>
-                <div>Cerca un Brawler</div>
-                <input type="search" name="search" id="search" />
+                <input
+                    type="search"
+                    name="search"
+                    id="search"
+                    value={searchQuery}
+                    onChange={(e) => SetSearchQuery(e.target.value)}
+                    placeholder="Cerca un Brawler ☺️🔎"
+                />
+                <button onClick={() => setSortOrder(sortOrder === "a-z" ? "z-a" : "a-z")}>
+                    {sortOrder === "a-z" ? "A-z⬆️" : "Z-a⬇️"}
+                </button>
             </div>
 
 

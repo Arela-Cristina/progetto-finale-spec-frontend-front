1b3dfc1de0d9793e22d3b9a9c97c8ea1cda4691e
Arela-Cristina <instagrandine@gmail.com>
2025-05-01 19:05:41 +0200

    feat: wrap component tree with SearchContext provider for global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { BrawlersProvider } from "./global/BrawlerProvider";
+import { SearchContextProvider } from "./global/SearchContext";
 import './App.css'
 import MainLayout from "./pages/MainLayout";
 import Home from "./pages/Home";
@@ -10,17 +11,18 @@ function App() {
 
 
   return (
-
     <BrawlersProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path="BrawlersList" element={<BrawlersList />} />
-            <Route path="FavoriteBrawlers" element={<FavoriteBrawlers />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <SearchContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Home />} />
+              <Route path="BrawlersList" element={<BrawlersList />} />
+              <Route path="FavoriteBrawlers" element={<FavoriteBrawlers />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </SearchContextProvider>
     </BrawlersProvider>
 
   );

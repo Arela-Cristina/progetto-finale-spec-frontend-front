c0c518b8ab774e48a71f37d3063c7e53e09ffce5
Arela-Cristina <instagrandine@gmail.com>
2025-05-01 19:11:06 +0200

    feat: connect with BrawlerContext and SearchContext using useContext in BrawlerList
    - Destructured context values
    - Mapped over the filteredBrawlers array coming from SearchContext

--- src/pages/BrawlersList.tsx
+++ src/pages/BrawlersList.tsx
@@ -1,25 +1,29 @@
 import { useContext } from "react"
 import BrawlersContext from "../global/BrawlersContext"
+import SearchContext from "../global/SearchContext"
 import { Brawler } from "../types/types"
 import style from './ui-pages-styles/BrawlerList.module.css'
 
 export default function BrawlersList() {
 
     const brawlerContext = useContext(BrawlersContext)
+    const searchBrawlerContext = useContext(SearchContext)
 
 
-    if (brawlerContext === null) {
-        return <div>Loading...</div>
+    if (brawlerContext === null || searchBrawlerContext === null) {
+        return null
     }
 
-    const { brawler } = brawlerContext
+    // const { brawler } = brawlerContext
+    const { filteredBrawlers } = useContext(SearchContext)!;
+   
 
     return (
 
         <section className={style.mainListContainer}>
             <h2>Lista di Brawlers</h2>
             <ul className={style.listContainer}>
-                {brawler.map((b: Brawler, id: number) => (
+                {filteredBrawlers.map((b: Brawler, id: number) => (
                     <li key={id} className={style.listElement}>
                         <figure>
                             <div>{b.title}</div>

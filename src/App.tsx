d5434a243f78618f8a5290fd2508edcd5808a10c
Arela-Cristina <instagrandine@gmail.com>
2025-05-02 15:26:43 +0200

    feat: add BrawlerDetail as a child route of BrawlerList to display as a modal - Preserves the main route while showing BrawlerDetail

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css'
 import MainLayout from "./pages/MainLayout";
 import Home from "./pages/Home";
 import BrawlersList from "./pages/BrawlersList";
+import BrawlerDetail from "./pages/BrawlerDetail";
 import FavoriteBrawlers from "./pages/FavoriteBrawlers";
 
 function App() {
@@ -18,6 +19,9 @@ function App() {
             <Route path="/" element={<MainLayout />}>
               <Route index element={<Home />} />
               <Route path="BrawlersList" element={<BrawlersList />} />
+              <Route path="BrawlersList" element={<BrawlersList />}>
+                <Route path=":id" element={<BrawlerDetail />} />
+              </Route>
               <Route path="FavoriteBrawlers" element={<FavoriteBrawlers />} />
             </Route>
           </Routes>

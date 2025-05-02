e12fb9d28b967b199a3ec787e5df0b5e329ec356
Arela-Cristina <instagrandine@gmail.com>
2025-05-02 19:01:34 +0200

    feat: create and add style on BrawlerDeatil page

--- src/pages/BrawlerDetail.tsx
+++ src/pages/BrawlerDetail.tsx
@@ -1,13 +1,16 @@
 import { useContext } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import BrawlersContext from "../global/BrawlersContext"
 import { Brawler } from "../types/types"
+import style from './ui-pages-styles/brawlerDetail.module.css'
 
 
 export default function BrawlerDetail() {
 
     const { id } = useParams() //mi ritorna una stringa
     const brawlerContext = useContext(BrawlersContext)
+
+    const navigate = useNavigate()
 
     if (!brawlerContext) {
         return null
@@ -22,35 +25,43 @@ export default function BrawlerDetail() {
 
     const { title, category, role, health, basicAttack, super: superAttack, gadgets, starPower, overdrive } = brawler
 
-    return (
-        <section>
-            <div>
-                <h3>{title}</h3>
-                <div>Brawler imagen</div>
-            </div>
-            <div>
-                <div>
-                    <p>Rarita: {category}</p>
-                    <p>Classe: {role}</p>
-                </div>
-                <div>
-                    <p>Salute: {health}</p>
-                    <p>Attaco: {basicAttack}</p>
-                    <p>Super: {superAttack}</p>
-                    <p>Overdrive: {overdrive}</p>
-                </div>
-                <div>
-                    <div>
-                        <h3>Gadgets</h3>
-                        {gadgets.map((g, i) => <p key={i}> {g} </p>)}
 
+    const onCloseBrawlerDetail = () => {
+        navigate(-1)
+    }
+
+    return (
+        <section className={style.modaleBrawler}>
+            <div className={style.modaleBrawlerDetailContainer}>
+                <div>
+                    <h3>{title}</h3>
+                    <div>Brawler imagen</div>
+                </div>
+                <div>
+                    <div>
+                        <p>Rarita: {category}</p>
+                        <p>Classe: {role}</p>
                     </div>
                     <div>
-                        <h3>Abilita Stellare</h3>
-                        {starPower.map((g, i) => <p key={i}> {g} </p>)}
+                        <p>Salute: {health}</p>
+                        <p>Attaco: {basicAttack}</p>
+                        <p>Super: {superAttack}</p>
+                        <p>Overdrive: {overdrive}</p>
                     </div>
+                    <div>
+                        <div>
+                            <h3>Gadgets</h3>
+                            {gadgets.map((g, i) => <p key={i}> {g} </p>)}
+
+                        </div>
+                        <div>
+                            <h3>Abilita Stellare</h3>
+                            {starPower.map((g, i) => <p key={i}> {g} </p>)}
+                        </div>
+                    </div>
+                    <button> Aggiungi ai Preferiti ❤️</button>
+                    <button onClick={onCloseBrawlerDetail}> Chiude ✖️ </button>
                 </div>
-                <button> Aggiungi ai Preferiti ❤️</button>
             </div>
         </section>
     )

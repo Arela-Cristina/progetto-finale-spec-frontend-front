45158065fed6971397b75c2f33c6d49b0ae7bb22
Arela-Cristina <instagrandine@gmail.com>
2025-05-05 19:14:45 +0200

    feat: implement useState to manage selected Brawler and comparison grid
    - Add BrawlerCard component to handle selection logic and UI
    - Implement grid system to display a comparison table for selected Brawlers

--- src/pages/BrawlersComparison.tsx
+++ src/pages/BrawlersComparison.tsx
@@ -1,6 +1,97 @@
+import { useContext, useState } from "react"
+import BrawlersContext from "../global/BrawlersContext"
+import { Brawler } from "../types/types"
+import BrawlerCard from "../components/BrawlerCard"
+import style from './ui-pages-styles/brawlerComparison.module.css'
+
 export default function BrawlersComparison() {
 
+
+    const brawlerContext = useContext(BrawlersContext)
+
+    if (brawlerContext === null) {
+        return null
+    }
+
+    const { brawler } = brawlerContext
+
+    //salviamo in uno stato i brawler scelti
+    const [selectedBrawler, setSelectedBrawler] = useState<Brawler[]>([])
+
+
+    const handleSelectBrawler = (valueSelectBrawler: Brawler) => {
+
+        if (selectedBrawler.some(b => b.id === valueSelectBrawler.id)) {
+
+            const deletedSelectedBarwler = selectedBrawler.filter(b => b.id !== valueSelectBrawler.id)
+
+            setSelectedBrawler(deletedSelectedBarwler)
+        } else {
+
+            if (selectedBrawler.length < 3) {
+                setSelectedBrawler([...selectedBrawler, valueSelectBrawler])
+            } else {
+
+                alert("¡Puoi selezionare solo fino a 3 combattenti! Per selezionarne un altro, è necessario deselezionarne uno tra quelli già selezionati..!");
+            }
+        }
+    }
+
     return (
-        <div>BrawlerComparison</div>
+        <>
+            <section className={style.mainMenuSelectContainer}>
+                <ul className={style.cardSelectContainer}>
+                    {brawler.map((b: Brawler, id: number) => (
+                        <li
+                            key={id}
+                            className={`${style.cardSelectElement} ${selectedBrawler.some(selected => selected.id === b.id)
+                                ? style.cardSelectedElement
+                                : ""
+                                }`}
+                            onClick={() => handleSelectBrawler(b)}
+
+                        >
+                            <BrawlerCard id={b.id} title={b.title} category={b.category} />
+                        </li>
+                    ))}
+                </ul>
+
+
+
+                <div className={style.comparisonGrid} data-columns={selectedBrawler.length}>
+                    {/* Fila title brawlers */}
+                    <div className={style.cell}></div> {/* Cella vuota */}
+                    {selectedBrawler.length > 0 && selectedBrawler.map((b, i) => (
+                        <div key={`title-${i}`} className={style.header}>{b.title}</div> // nomi brawlers column
+                    ))}
+
+                    {/* show fila si...  */}
+                    {selectedBrawler.length > 0 && (
+                        <>
+                            {/* Fila: Health */}
+                            <div className={style.label}>Health</div>
+                            {selectedBrawler.map((b, i) => (
+                                <div key={`health-${i}`} className={style.value}>{b.health}</div> // Colum Health 
+                            ))}
+
+                            {/* Fila: Basic Attack */}
+                            <div className={style.label}>Basic Attack</div>
+                            {selectedBrawler.map((b, i) => (
+                                <div key={`attack-${i}`} className={style.value}>{b.basicAttack}</div> //  Colum Basic Attack
+                            ))}
+
+                            {/* Fila: Overdrive */}
+                            <div className={style.label}>Overdrive</div>
+                            {selectedBrawler.map((b, i) => (
+                                <div key={`over-${i}`} className={style.value}>{b.overdrive}</div> // Column Overdrive
+                            ))}
+                        </>
+                    )}
+                </div>
+
+
+
+            </section>
+        </>
     )
 }

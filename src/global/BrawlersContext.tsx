2b36f45a9ee9a196c993552cdb6ca8cb4aeec16d
Arela-Cristina <instagrandine@gmail.com>
2025-05-06 15:49:32 +0200

    fix: correct addToFavorites type to accept a single Brawler instead of an array

--- src/global/BrawlersContext.tsx
+++ src/global/BrawlersContext.tsx
@@ -5,9 +5,9 @@ import type { Brawler } from '../types/types';
 export interface BrawlersContextType {
     brawler: Brawler[];
     setBrawler: (value: Brawler[]) => void;
-    favotites: Brawler[];
-    addToFavorites: (value: Brawler[]) => void;
-    removeFromFavorites: (idValue: number) => void;
+    favorites: Brawler[];
+    addToFavorites: (value: Brawler) => void;
+    removeFromFavorites: (id: number) => void;
 }
 
 const BrawlersContext = createContext<BrawlersContextType | null>(null);

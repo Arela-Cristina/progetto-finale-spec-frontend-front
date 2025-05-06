c19898d302e413797fe54f77d5926b494c0b10c6
Arela-Cristina <instagrandine@gmail.com>
2025-05-06 16:52:32 +0200

    feat: import useContext, BrawlerContext, and Brawler type to render Favorite Brawlers page
    - Implement Link from react-router-dom to redirect to Detail page

--- src/pages/FavoriteBrawlers.tsx
+++ src/pages/FavoriteBrawlers.tsx
@@ -1,5 +1,40 @@
+import { useContext } from "react"
+import BrawlersContext from "../global/BrawlersContext"
+import { Brawler } from "../types/types"
+import style from './ui-pages-styles/FavoriteBrawlers.module.css'
+import { Link } from "react-router-dom"
+
+
+
 export default function FavoriteBrawlers() {
-    return(
-        <div>My Favorite Brawlers</div>
+
+    const favoriteBrawlersContext = useContext(BrawlersContext);
+
+    if (!favoriteBrawlersContext) {
+        return null
+    }
+
+    const { favorites } = favoriteBrawlersContext
+
+    return (
+        <section className={style.mainContainer}>
+            {favorites.length === 0 ? (
+                <>
+                    <p>Ancora non hai un Brawler preferito 😄. </p>
+                    <p>Aggiungi qualcuno dalla Pagina di Detaglio della Lista Brawlers 😊</p>
+                </>
+
+            ) : (
+                <ul className={style.favoritesList}>
+                    {favorites.map((brawler: Brawler) => (
+                        <li key={brawler.id} className={style.listItem}>
+                            <Link to={`/BrawlersList/${brawler.id}`}>
+                                <h3>{brawler.title}</h3>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </section>
     )
 }

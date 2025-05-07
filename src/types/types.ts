851f8173d7c1ca7bd5310d86294738cfea599f2b
Arela-Cristina <instagrandine@gmail.com>
2025-05-07 16:25:01 +0200

    add: new property to type.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,6 +2,7 @@ export type Brawler = {
     id: number,
     title: string, //nome brawler
     category: string, // rarity brawler
+    image: string,
     role: string, // cecchino, tank, assasino ... 
     health: number, //salute brawler
     basicAttack: string, // basico

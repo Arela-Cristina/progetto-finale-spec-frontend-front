164c446b6588779ca0d6e658ca9002899c3c4151
Arela-Cristina <instagrandine@gmail.com>
2025-05-08 15:25:18 +0200

    add: style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,25 @@
+import pochoImage from '../assets/gif/pocho.svg'
+import istruzioniTitle from '../assets/titles/istruzioni-titolo.svg'
+
 export default function Home() {
     return (
-        <div>This is Home Page</div>
+        <section className="flex gap-[4rem] m-auto w-[90%] bg-pink-500 justify-center rounded-b-lg shadow-lg pb-[3rem]">
+
+            <div className="w-[40rem]">
+                <div>
+                    <img src={istruzioniTitle} alt="instruccions title" />
+                </div>
+                <div>
+                    <p>Devi fare questo</p>
+                    <p>Devi fare questo</p>
+                    <p>Devi fare questo</p>
+                    <p>Devi fare questo</p>
+                </div>
+            </div>
+
+            <div className="w-[32rem] mt-[8rem]">
+                <img src={pochoImage} alt="pocho" />
+            </div>
+        </section>
     )
 }

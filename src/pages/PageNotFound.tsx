d33ff626aed6f86e9b06c77b8b52b248687ebc33
Arela-Cristina <instagrandine@gmail.com>
2025-05-12 19:07:59 +0200

    feat: create PageNoutFound Page

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,12 @@
+import { useNavigate } from "react-router-dom";
 export default function PageNotFound() {
+
+    const navigate = useNavigate()
     return (
-        <div>Pagina non trovata</div>
+        <div className="text-center p-10">
+            <h1 className="text-3xl font-bold mb-4">404 - Página non trovata</h1>
+            <p>Ci dispiace questo sito non esiste, vuoi tornare nella Home 😀 ?.</p>
+            <button onClick={() => navigate("/")} className="bg-pink-600 text-white px-4 py-2 mt-4 rounded hover:bg-pink-700 transition">Home</button>
+        </div>
     )
 }

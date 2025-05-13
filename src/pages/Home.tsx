2b6e1d8d2838232275c72a9c6d3ecb74e6dee1ba
Arela-Cristina <instagrandine@gmail.com>
2025-05-13 16:54:10 +0200

    add: style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,17 +3,17 @@ import istruzioniTitle from '../assets/titles/istruzioni-titolo.svg'
 
 export default function Home() {
     return (
-        <section className="flex gap-[4rem] m-auto w-[90%] bg-pink-500 justify-center rounded-b-lg shadow-lg pb-[3rem]">
+        <section className="flex gap-[4rem] m-auto w-[90%] bg-[#282039b3] justify-center rounded-b-lg shadow-lg pb-[3rem]">
 
             <div className="w-[40rem]">
                 <div>
                     <img src={istruzioniTitle} alt="instruccions title" />
                 </div>
-                <div>
-                    <p>Devi fare questo</p>
-                    <p>Devi fare questo</p>
-                    <p>Devi fare questo</p>
-                    <p>Devi fare questo</p>
+                <div className="space-y-4 max-w-lg mx-auto p-6">
+                    <p className="text-lg text-white font-semibold">💡 Accedi a qualsiasi sezione del sito dal Menu in alto</p>
+                    <p className="text-lg text-white font-semibold">💡 Cerca e ordina tutti i tipi di Brawler</p>
+                    <p className="text-lg text-white font-semibold">💡 Seleziona e salva i tuoi Brawler Preferiti</p>
+                    <p className="text-lg text-white font-semibold">💡 Confronta fino a 3 Brawler</p>
                 </div>
             </div>
 
